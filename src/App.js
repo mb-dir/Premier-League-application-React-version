9695d148df4e5dde8ac072f9830a068c75ae5397
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-05-02 22:02:20 +0200

    Static App content
    Static header content

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <main className="App">
       <header className="App-header">
-        <h1>Hello another React app</h1>
+        <h1>Premier League Clubs</h1>
+        <p>
+          Simple application which shows basic information about Premier League
+          clubs like stadion, date of creation, current players, etc.
+        </p>
       </header>
-    </div>
+      {/* Here will be slider with all PL clubs and special section with info about clicked club */}
+    </main>
   );
 }
 
