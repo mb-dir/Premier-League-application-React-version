5f273a9b7679a0b045a09b39b2c2d94e5e88f3fc
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-05-03 21:08:15 +0200

    Try to implement "roll" logic
    It turned out that scrollTo doesn't work in React - I have found the
    solution https://stackoverflow.com/questions/43441856/how-to-scroll-to-an-element
    but so far I have not learnt about useRef, in this project I want to use
    only what I have learnt so far, so I will have to give up with "roll"
    feature, insted of this I will create just normal list

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -50,9 +50,26 @@ export default function Slider() {
     </div>
   );
 
+  //Default scrollPos is 0, each "prev"/"next" click will update it
+  const [ scrollPos, setScrollPos ] = React.useState(0);
+
+  //When user clicks "prev" btn change position
+  function scrollLeft() {
+    setScrollPos(prevPos => {
+      return prevPos - 100;
+    });
+    //I turned out that it doesn't work in React, I have found the solution(https://stackoverflow.com/questions/43441856/how-to-scroll-to-an-element), but it uses useRef, I haven't learnt about it so far and in this project I want to use only what I have learnt so far
+    roll.scrollTo({
+      left: scrollPos,
+      behavior: "smooth",
+    });
+  }
+
   return (
     <div className="Slider">
-      <button className="Slider-btn">prev</button>
+      <button onClick={scrollLeft} className="Slider-btn">
+        prev
+      </button>
       <div className="Slider-wrapper">{roll}</div>
       <button className="Slider-btn">next</button>
     </div>
