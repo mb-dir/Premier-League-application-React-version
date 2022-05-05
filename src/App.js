7bfb7329d3ba54026ca17709d67e1a9526869b70
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-05-05 22:45:38 +0200

    Getting info about clubs from API
    Based on this info the slider will be rendered

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React from "react";
 import "./App.css";
 
 import Slider from "./components/Slider/Slider";
 import ClubInfo from "./components/ClubInfo/ClubInfo";
 
 function App() {
+  const API_KEY = "https://api.football-data.org/v2/competitions/PL/teams";
+
+  const [ clubsInfo, setClubsInfo ] = React.useState([]);
+  React.useEffect(() => {
+    fetch(API_KEY, {
+      method: "GET",
+      headers: {
+        "X-Auth-Token": "b100821898ab4506af51fd31aa51125e",
+      },
+    })
+      .then(clubs => clubs.json())
+      //Array of objects - each object has info about concrete PL team
+      .then(clubs => setClubsInfo(clubs.teams));
+  }, []);
+
   return (
     <main className="App">
       <header className="App-header">
