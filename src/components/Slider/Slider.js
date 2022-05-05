ec6477855207896bc3ddfb00397c54530bdb9d1c
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-05-05 23:24:14 +0200

    Use map to render crest insted of hard-coding

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -16,109 +16,24 @@ export default function Slider() {
       .then(clubs => setClubsInfo(clubs.teams));
   }, []);
 
+  const clubsCrest = clubsInfo.map(club => {
+    const { crestUrl, name, id } = club;
+    return (
+      <img
+        key={id}
+        src={crestUrl}
+        className="Slider-crest"
+        alt={`The crest of ${name}`}
+      />
+    );
+  });
+
   return (
     <section className="Slider">
       <h2 className="Slider-header">
         Click the crest to see more info about the club
       </h2>
-      <div className="Slider-wrapper">
-        {/* All below items will be dynamically created based on API resposne and map method */}
-        <img
-          className="Slider-crest"
-          src="https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg"
-          alt="The crest of the best football club in the World;)"
-        />
-        <img
-          className="Slider-crest"
-          src="https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg"
-          alt="The crest of the best football club in the World;)"
-        />
-        <img
-          className="Slider-crest"
-          src="https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg"
-          alt="The crest of the best football club in the World;)"
-        />
-        <img
-          className="Slider-crest"
-          src="https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg"
-          alt="The crest of the best football club in the World;)"
-        />
-        <img
-          className="Slider-crest"
-          src="https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg"
-          alt="The crest of the best football club in the World;)"
-        />
-        <img
-          className="Slider-crest"
-          src="https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg"
-          alt="The crest of the best football club in the World;)"
-        />
-        <img
-          className="Slider-crest"
-          src="https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg"
-          alt="The crest of the best football club in the World;)"
-        />
-        <img
-          className="Slider-crest"
-          src="https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg"
-          alt="The crest of the best football club in the World;)"
-        />
-        <img
-          className="Slider-crest"
-          src="https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg"
-          alt="The crest of the best football club in the World;)"
-        />
-        <img
-          className="Slider-crest"
-          src="https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg"
-          alt="The crest of the best football club in the World;)"
-        />
-        <img
-          className="Slider-crest"
-          src="https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg"
-          alt="The crest of the best football club in the World;)"
-        />
-        <img
-          className="Slider-crest"
-          src="https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg"
-          alt="The crest of the best football club in the World;)"
-        />
-        <img
-          className="Slider-crest"
-          src="https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg"
-          alt="The crest of the best football club in the World;)"
-        />
-        <img
-          className="Slider-crest"
-          src="https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg"
-          alt="The crest of the best football club in the World;)"
-        />
-        <img
-          className="Slider-crest"
-          src="https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg"
-          alt="The crest of the best football club in the World;)"
-        />
-        <img
-          className="Slider-crest"
-          src="https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg"
-          alt="The crest of the best football club in the World;)"
-        />
-        <img
-          className="Slider-crest"
-          src="https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg"
-          alt="The crest of the best football club in the World;)"
-        />
-        <img
-          className="Slider-crest"
-          src="https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg"
-          alt="The crest of the best football club in the World;)"
-        />
-        <img
-          className="Slider-crest"
-          src="https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg"
-          alt="The crest of the best football club in the World;)"
-        />
-      </div>
+      <div className="Slider-wrapper">{clubsCrest}</div>
     </section>
   );
 }
