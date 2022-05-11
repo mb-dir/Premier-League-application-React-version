120d539c7eea3271a3ff705b7a35bbcb2e5c01d9
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-05-11 21:32:33 +0200

    Transfer fetch from Slider to App
    Now App is responsible for getting data, and Slider is responsible only
    for rendering the content(I have to implement it)

--- src/App.js
+++ src/App.js
@@ -7,6 +7,21 @@ import ClubInfo from "./components/ClubInfo/ClubInfo";
 function App() {
   const [ clubID, setClubID ] = React.useState(0);
 
+  //Getting all teams for API
+  const [ clubsInfo, setClubsInfo ] = React.useState([]);
+  const API_KEY = "https://api.football-data.org/v2/competitions/PL/teams";
+  React.useLayoutEffect(() => {
+    fetch(API_KEY, {
+      method: "GET",
+      headers: {
+        "X-Auth-Token": "b100821898ab4506af51fd31aa51125e",
+      },
+    })
+      .then(clubs => clubs.json())
+      //Array of objects - each object has info about concrete PL team
+      .then(clubs => setClubsInfo(clubs.teams));
+  }, []);
+
   //https://stackoverflow.com/questions/29100774/reactjs-setstate-on-parent-inside-child-component
   //In order to pass setState to child I have to create "wrapper" function for it which is passed as props
   function settingID(id) {
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,21 +1,6 @@
 import React from "react";
 import "./Slider.css";
 export default function Slider(settingID) {
-  const API_KEY = "https://api.football-data.org/v2/competitions/PL/teams";
-
-  const [ clubsInfo, setClubsInfo ] = React.useState([]);
-  React.useEffect(() => {
-    fetch(API_KEY, {
-      method: "GET",
-      headers: {
-        "X-Auth-Token": "b100821898ab4506af51fd31aa51125e",
-      },
-    })
-      .then(clubs => clubs.json())
-      //Array of objects - each object has info about concrete PL team
-      .then(clubs => setClubsInfo(clubs.teams));
-  }, []);
-
   //Update state in App, thanks to this App knows which club(id) was clicked, and App can pass this info to ClubInfo component to render the info abou clicked club
   function setID(id) {
     settingID.settingID(id);
